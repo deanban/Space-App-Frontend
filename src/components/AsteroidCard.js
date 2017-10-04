501e3ed258abeed19a7e5d2d09658679ad62967a
deanban <pbp2005@gmail.com>
2017-10-04 10:09:25 -0400

    mvp part 2

--- src/components/AsteroidCard.js
+++ src/components/AsteroidCard.js
@@ -5,39 +5,46 @@ import React from 'react'
 class AsteroidCard extends React.Component{
 
 
-
 	render(){
-		return(
-  				<div class="card">
-    				<div class="content">
-      					<div class="header">
-      						{this.props.name}
-      					</div>
-  						<div class="meta">
-  							<a href={this.props.url}>More Info.</a>
-  						</div>
-						<div class="Hazardous">
-							Hazardous: {this.props.hazardous.toString()}
-						</div>
-						<div class="Miss Distance(mile)">
-							Miss Distance(mile): {this.props.miss_distance}
-						</div>
-						<div class="Approach Date">
-							Approach Date: {this.props.approach_date}
-						</div>
-						<div class="Relative Velocity">
-							Relative Velocity(mph): {Math.round(this.props.relative_velocity)}
-						</div>
-						<div class="Min est range">
-							Min est size(feet): {Math.round(this.props.est_diameter_min)}
-						</div>
-						<div class="Max est range(feet)">
-							Max est size(feet): {Math.round(this.props.est_diameter_max)}
-						</div>
-    				</div>
-    			</div>
-	)}
+				
+				return(
+	  				<div class="card">
+	    				<div class="content">
+	      					<div class="header">
+	      						{this.props.name}
+	      					</div>
+	  						<div class="meta">
+	  							<a href={this.props.url}>More Info.</a>
+	  						</div>
+
+									<div class="Hazardous">
+										Hazardous: {this.props.hazardous.toString()}
+									</div>
+									<div class="Miss Distance(mile)">
+										Miss Distance(mile): {this.props.miss_distance}
+									</div>
+									<div class="Approach Date">
+										Approach Date: {this.props.approach_date}
+									</div>
+									<div class="Relative Velocity">
+										Relative Velocity(mph): {Math.round(this.props.relative_velocity)}
+									</div>
+									<div class="Min est range">
+										Min est size(feet): {Math.round(this.props.est_diameter_min)}
+									</div>
+									<div class="Max est range(feet)">
+										Max est size(feet): {Math.round(this.props.est_diameter_max)}
+									</div>
+								</div>
+	    				</div>			
+	    			
+				)
+		
+		
+	}			
+
 }
 
 
 export default AsteroidCard
+
--- src/components/AsteroidContainer.js
+++ src/components/AsteroidContainer.js
@@ -1,27 +1,113 @@
 import React from 'react'
 import AsteroidCard from './AsteroidCard'
-import StackGrid from "react-stack-grid"
 
 
 class AsteroidContainer extends React.Component{
 
 	state = {
-		data: []
+		data: [],
+		filteredStr: 'all',
+		sortSize: ''
 	}
 
 
 	componentDidMount(){
 		fetch("http://localhost:3000/asteroids")
 			.then( resp => resp.json() )
-			.then( resp => this.setState({
-				data: resp
+			.then( respData => this.setState({
+				data: respData
 			}))
 	}
 
+
+	handleChange = (event) =>{
+		// let haz = this.filterByHazard(event.target.value)
+		this.setState({
+			filteredStr: event.target.value
+		})
+
+		console.log(event.target.value)
+		
+	}
+
+	handleSortChange = (event) => {
+		this.setState({
+			sortSize: event.target.value
+		})
+
+		console.log(event.target.value)
+
+		
+	}
+
+	filterByHazard = () => {
+		let newData = this.sortData()
+		
+		console.log(this.state.data)
+		console.log(this.state.data.filter(filterHaz => filterHaz.hazardous.toString() === this.state.filteredStr))
+		
+		if(this.state.filteredStr === 'all'){
+			return newData
+		}
+		
+		return newData.filter(filterHaz => filterHaz.hazardous.toString() === this.state.filteredStr)
+		
+		
+	}
+
+	sortData = () => {
+
+		if (this.state.sortSize === 'asc') {
+
+			let newObj = this.state.data
+			
+			return newObj.sort(function(a,b) {return a.estimated_diameter_max - b.estimated_diameter_max})
+		} 
+		else if (this.state.sortSize === 'desc') {
+			let newObj = this.state.data
+			
+			return newObj.sort(function(a,b) {return b.estimated_diameter_max - a.estimated_diameter_max})
+		} 
+		else {
+			return this.state.data
+		}		
+	}
+
+
+	selectBox = () =>{
+		return(
+			<select class="ui dropdown" onChange={this.handleChange}>
+				<option value="" disabled selected>Sort by hazardous</option>
+				<option value="all">All</option>
+	  			<option value="false">False</option>
+	  			<option value="true">True</option>
+			</select>
+			)
+	}
+
+	sizeBox = () =>{
+		return(
+			<select class="ui dropdown" onChange={this.handleSortChange}>
+				<option value="" disabled selected>Sort by Maximum size</option>
+	  			<option value="asc">Ascending</option>
+	  			<option value="desc">Descending</option>
+			</select>
+			)
+	}
+
+
+
 	render(){
-		return( <div class="ui cards">
+		return( 
+				<div>
+					{this.selectBox()}
+					{this.sizeBox()}
+					<div class="ui cards">
+
 					
-						{this.state.data.map(element => <AsteroidCard 
+						{this.filterByHazard().map(element => <AsteroidCard
+							
+							filterBy={this.state.filteredStr}
 							name = {element.name}
 							url = {element.nasa_jpl_url}
 							hazardous = {element.hazardous}
@@ -33,7 +119,9 @@ class AsteroidContainer extends React.Component{
 							/>
 						)}
 				
-				</div>)
+					</div>
+				</div>
+			)
 	}
 }
 
